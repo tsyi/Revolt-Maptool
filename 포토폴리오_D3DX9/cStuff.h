325259db53aa666314cf61ab9b2b8a224b202808
LEEGB-SURFACE\Lee Geon Bae <kolzkr@naver.com>
2017-10-24 18:35:05 +0900

    기즈모 생성

--- 포토폴리오_D3DX9/cStuff.h
+++ 포토폴리오_D3DX9/cStuff.h
@@ -9,28 +9,11 @@ public:
 
 	void Update()
 	{
+//		SetMeshBox();
 		cObject::Update();
 	}
 	void Render()
 	{
 		cObject::Render();
-	}
-	void SetMeshBox()
-	{
-		D3DXCreateBox(MgrD3DDevice, 1, 1, 1, &(GetMeshData()->m_pMesh), NULL);
-
-		D3DMATERIAL9 material;
-		ZeroMemory(&material, sizeof(D3DMATERIAL9));
-		D3DXCOLOR materColor = D3DXCOLOR(0.3f, 1.0f, 0.8f, 1.0f);
-		material.Ambient = materColor;
-		material.Diffuse = materColor;
-		material.Specular = materColor;
-
-		cMtlTex* mtlText = new cMtlTex;
-
-		mtlText->SetMaterial(material);
-		mtlText->SetMtlTexID(0);
-
-		GetMeshData()->m_vecMtlTex.push_back(mtlText);
 	}
 };
